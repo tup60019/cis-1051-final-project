adadaf3e66718c56aabf81ff4e0eed0935887203
puddingdee <129347801+tup60019@users.noreply.github.com>
2023-04-27 00:31:59 -0400

    finished hills

--- Assets/Scripts/HillsClimberController.cs
+++ Assets/Scripts/HillsClimberController.cs
@@ -1,60 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
-public class MountainClimberController : MonoBehaviour
+public class HillsClimberController : MonoBehaviour
 {
+    public float cooldown;
     public ClimberController climber;
+    public Animation duck;
     public MainManager mainManager;
-    public float horizInput;
-    private int climbSpeed = 10000;
-    private float rBound = 4.5f;
-    private float lBound = -4.5f;
-    public string climberPos = "C";
     public float redTime = 0;
     // Start is called before the first frame update
     void Start()
     {
+        cooldown = -1;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizInput = Input.GetAxis("Horizontal");
+
         if (climber.climberIsAlive)
         {
-
-            transform.Translate(horizInput * Vector2.right * climbSpeed * Time.deltaTime);
-            if (transform.position.x >= rBound)
+            
+            if (Input.GetKeyDown(KeyCode.Space) && cooldown <= 0)
             {
-                transform.position = new Vector3(rBound, transform.position.y, transform.position.z);
-                climberPos = "R";
+                GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(0, 1000));
+                cooldown = 0.71f;
+                
             }
-
-            if (transform.position.x <= lBound)
+            if (cooldown > 0)
             {
-                transform.position = new Vector3(lBound, transform.position.y, transform.position.z);
-                climberPos = "L";
+                cooldown -= Time.deltaTime;
             }
-
-
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                duck.Play();
+            }
+            
         }
+        //on hit red effect REMEMBER TO INCLUDE i IN COLLISION
         if (redTime > 0)
         {
             Color c = GetComponent<SpriteRenderer>().color = Color.red;
-
+            
         }
         redTime -= Time.deltaTime;
         if (redTime <= 0)
         {
             Color g = GetComponent<SpriteRenderer>().color = Color.white;
         }
+        
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Boulder(Clone)")
+        
+        if (collision.gameObject.CompareTag("HillsEnemy"))
         {
             mainManager.mistakes++;
             redTime = 0.7f;
